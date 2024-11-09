a4be7036e702d2a0e2c83328fab53a54ec1e327d
Aditya Pratama <aditypraa@gmail.com>
2024-11-09 18:56:20 +0700

    refactor(sturktur folder)

--- backend/src/app/config/database.js
+++ backend/src/app/config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
   development: {
-    username: "aditypraa",
-    password: "aditypraa",
-    database: "clone_trello",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: process.env.PORT,
   },
   test: {
     username: "root",
